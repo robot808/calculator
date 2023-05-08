dd3d00c467afc0e557b960835260e7da57affe93
robot808 <rsbradshaw88@protonmail.com>
2023-05-08 18:39:12 -0500

    Add functions for basic mathematical operations

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+function add (numberA, numberB) {
+  return numberA + numberB;
+}
+
+function subtract (numberA, numberB) {
+  return numberA - numberB;
+}
+
+function multiply (numberA, numberB) {
+  return numberA * numberB;
+}
+
+function divide (numberA, numberB) {
+  return numberA / numberB;
+}
+
 const html = document.documentElement;
 const buttons = document.querySelectorAll(".button");
 
